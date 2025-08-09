3ddf595582f6084bbd38c8795936e55a09ab00cd
Cătălina <ratacatalina16@gmail.com>
2025-08-09 17:07:44 +0300

    retusari

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -24,7 +24,7 @@ const Index = () => {
 
   return (
     <section className='bg-img h-screen'>
-    <section className=" flex items-start md:items-center justify-center pt-[50%] md:pt-[2%] px-0.5">
+    <section className=" flex items-start md:items-center justify-center pt-[50%] md:pt-0 px-0.5 md:px-0">
       <Swiper
         slidesPerView={1}
         spaceBetween={0}
@@ -59,7 +59,7 @@ const Index = () => {
       </Swiper>
     </section>
     
-    <div className='relative md:-right-11/12 cursor-pointer object-bottom-right -right-4/5 pt-18 md:pt-0 '>
+    <div className='relative md:-right-11/12 md:bottom-30 cursor-pointer -right-4/5 pt-18 md:pt-0'>
     <div className="bg-[#57C785] w-16 h-16 md:w-24 md:h-24 rounded-full flex items-center justify-center shadow-lg shadow-[#57C785] hover:md:w-[104px] hover:md:h-[104px] hover:w-20 hover:h-20 duration-75 " >
       <button className='text-white md:text-4xl text-2xl'><FaExclamation /></button>
     </div>
